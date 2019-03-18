cfb7f1e9cb4f384e0225ba3e0b18c746ee6361fb
Alex Ilchenko <alex.ilchenko@blueprint-london.com>
2019-03-18 09:40:31 +0100

    eslint, get rid of babel

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-function smartSize(size, { unit, scale } = {}) {
-  if (typeof size === 'string') {
-    return size
-  } else if (typeof size === 'number') {
-    return `${size * (scale == null ? smartSize.defaults.scale : scale)}${
-      unit == null ? smartSize.defaults.unit : unit
-    }`
-  } else if (Array.isArray(size)) {
-    return size.map(v => smartSize(v, { unit, scale })).join(' ')
-  } else if (size && typeof size === 'object') {
-    return smartSize(
-      [
-        size.top || size.vertical,
-        size.right || size.horizontal,
-        size.bottom || size.vertical,
-        size.left || size.horizontal
-      ],
-      { unit, scale }
-    )
+;(() => {
+  function smartSize (size, { unit, scale } = {}) {
+    if (typeof size === 'string') {
+      return size
+    } else if (typeof size === 'number') {
+      return `${size * (scale == null ? smartSize.defaults.scale : scale)}${
+        unit == null ? smartSize.defaults.unit : unit
+      }`
+    } else if (Array.isArray(size)) {
+      return size.map(v => smartSize(v, { unit, scale })).join(' ')
+    } else if (size && typeof size === 'object') {
+      return smartSize(
+        [
+          size.top || size.vertical,
+          size.right || size.horizontal,
+          size.bottom || size.vertical,
+          size.left || size.horizontal
+        ],
+        { unit, scale }
+      )
+    }
+    return 0
   }
-  return 0
-}
 
-smartSize.defaults = { unit: 'em', scale: 1 }
+  smartSize.defaults = { unit: 'em', scale: 1 }
 
-module.exports = smartSize
+  module.exports = smartSize
+})()
--- test/index.js
+++ test/index.js
@@ -4,37 +4,37 @@ const smartSize = require('../')
 
 const smartSizeDefaults = require('../')
 
-describe('smartSize', function() {
-  it('should return 0 when no params passed', function() {
+describe('smartSize', function () {
+  it('should return 0 when no params passed', function () {
     assert.equal(smartSize(), 0)
   })
-  it('should return same value when string passed', function() {
+  it('should return same value when string passed', function () {
     assert.equal(smartSize('10px'), '10px')
   })
-  it('should return 1em when number passed', () => {
+  it('should return ${n}em when number passed', () => {
     assert.equal(smartSize(1), '1em')
   })
-  it('should return string with 4 values when object passed', () => {
+  it('should return string with 4 values (css syntax for top, right, bottom, left) when object passed', () => {
     assert.equal(smartSize({ top: 1, bottom: 2, left: '1px' }), '1em 0 2em 1px')
   })
-  it('should return string when array passed', () => {
+  it('should return string when array passed with same values amount', () => {
     assert.equal(smartSize([1, 2]), '1em 2em')
   })
   it('should return string with 4 values when vertical and horizontal values passed', () => {
     assert.equal(smartSize({ vertical: 1, horizontal: 2 }), '1em 2em 1em 2em')
   })
-  it('should return 1px when number and unit passed', () => {
+  it('should return use provided units', () => {
     assert.equal(smartSize(1, { unit: 'px' }), '1px')
   })
-  it('should return 2px when number, unit and scale passed', () => {
-    assert.equal(smartSize(1, { unit: 'px', scale: 1 }), '1px')
+  it('should return use provided scale', () => {
+    assert.equal(smartSize(1, { scale: 2 }), '2em')
   })
 })
 
 describe('smartSize.defaults', () => {
   it('should use defaults', () => {
-    smartSizeDefaults.defaults.unit = 'px'
+    smartSizeDefaults.defaults.unit = 'rem'
     smartSizeDefaults.defaults.scale = 2
-    assert.equal(smartSizeDefaults(1), '2px')
+    assert.equal(smartSizeDefaults(1), '2rem')
   })
 })
